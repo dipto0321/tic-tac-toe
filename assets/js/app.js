e144030ff9176870493d948aee5b674ffd8ad503
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-02-12 11:30:37 +0800

    Include squre pushing into moves arr functionality to player clicking a square

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,29 @@
+// Winning combination:
+const winningCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
 // Collect all cells
 let cells = [...document.getElementsByClassName("cell")];
 
+let demoPlayer = new Player("human", "X")
+
 // Add click functionality
-cells.forEach(cell => cell.addEventListener("click", squareInnerText));
+cells.forEach(cell => cell.addEventListener("click", e => squareInnerText(e, demoPlayer, winningCombo)));
 
 // Change the inner text of box
 
-function squareInnerText(e) {
+function squareInnerText(e, player, winArr) {
   e.stopPropagation();
-  e.target.innerText = e.target.id;
+  e.target.innerText = player.sign;
+  player.turn(e.target.id, winArr);
 }
 
 document.getElementById("reset").addEventListener("click", clearTable);
--- assets/js/player.js
+++ assets/js/player.js
@@ -2,5 +2,34 @@ class Player {
   constructor(name, sign) {
     this.name = name;
     this.sign = sign;
+    this.moves = [];
+  }
+  turn(squareID, winArr) {
+    // Player choosing a square
+    // Whatever square the player chooses will be pushed into his moves array
+    this.moves.push(Number(squareID));
+    // Checking if player's moves array will have any winning combination -> win
+    winArr.forEach(combo => {
+      combo.every(val => this.moves.includes(val)) ? console.log("You won") : console.log("Keep playing");
+    })
+    // If none then checking if no more empty space in the board -> draw
+  }
+}
+
+class Human extends Player {
+  constructor(name, sign) {
+    super(name, sign)
+  }
+  turn() {
+    super.turn;
+  }
+}
+
+class Computer extends Player {
+  constructor(name, sign) {
+    super(name, sign)
+  }
+  turn() {
+    super.turn;
   }
 }
