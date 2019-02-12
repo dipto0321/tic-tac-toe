fd38d394587dabcb55b813272abf8b4f22ef59d7
dipto0321 <diptokmk47@gmail.com>
2019-02-12 08:54:08 +0600

    Add functionality to clear cell data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,20 @@
 // Collect all cells
-[...document.getElementsByClassName("cell")].forEach(cell => cell.addEventListener("click", squareInnerText));
+let cells = [...document.getElementsByClassName("cell")];
+
+// Add click functionality
+cells.forEach(cell => cell.addEventListener("click", squareInnerText));
 
 // Change the inner text of box
 
 function squareInnerText(e) {
   e.stopPropagation();
   e.target.innerText = e.target.id;
+}
+
+document.getElementById("reset").addEventListener("click", clearTable);
+
+// Reset table
+
+function clearTable() {
+  cells.forEach(cell => cell.innerText = "");
 }
