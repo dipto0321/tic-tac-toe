4d0b3f0a72d43854eeba35842662153d18a4d44f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-02-13 10:55:45 +0800

    Refactor gameCycle to remove duplication w/ player turns code

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -8,26 +8,32 @@ function cellFunctionality(e) {
   gameCycle(e.target, winningCombo);
 }
 
-// Change the inner text of box
-
+// One cycle in a game means human and computer each makes a turn
 function gameCycle(target, winArr) {
-  let humanChoice = target;
-  humanChoice.innerText = human.sign;
-  human.turn(winArr, humanChoice.id);
-  humanChoice.removeEventListener("click", cellFunctionality);
+  // First human chooses a square
+  playerTurn(winArr, target, human);
+  // If game isn't a draw then the computer will choose a square
   if (!checkDraw(computer)) {
-    let computerChoice = document.getElementById(String(computer.makeChoice()));
-    computer.turn(winArr, computerChoice.id);
-    computerChoice.innerText = computer.sign;
-    computerChoice.removeEventListener("click", cellFunctionality);
+    playerTurn(winArr, target, computer);
   }
+  // If either player wins, remove all click functionality from remaining cells
   if (checkWin(winArr, computer) || checkWin(winArr, human)) {
     cells.forEach(cell => cell.removeEventListener("click", cellFunctionality));
   }
 }
 
-// Reset
+function playerTurn(winArr, target, player) {
+  // playerChoice will differ depending on type of player
+  let playerChoice = player === human ? target : document.getElementById(String(player.makeChoice()));
+  // Player makes a turn
+  player.turn(winArr, playerChoice.id);
+  // Change inner text of the chosen square into the player's sign
+  playerChoice.innerText = player.sign;
+  // After the square is chosen remove click functionality from it
+  playerChoice.removeEventListener("click", cellFunctionality);
+}
 
+// Reset
 function resetGame() {
   cells.forEach(cell => (cell.innerText = ""));
   game = new Game();
