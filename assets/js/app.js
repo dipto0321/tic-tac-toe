4ca026a3d879788a11c61aaa55f282c219f38b6f
dipto0321 <diptokmk47@gmail.com>
2019-02-19 15:09:45 +0600

    Create main board from app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,23 @@
+(
+  ()=>{
+    let mainGameBoard = document.getElementById("mainBoard");
+    // let j = 0;
+    for (let i = 0, j=0; i < 3; i+=1, j+=3) {
+      let tr = document.createElement("tr");
+      for (let i = 0; i < 3 ; i+=1) {
+        let td = document.createElement("td");
+        td.setAttribute("class", "cell");
+        td.setAttribute("id",j+i);
+        tr.appendChild(td);
+      }
+      mainGameBoard.appendChild(tr);
+    }
+  }
+)();
+
 // Collect all cells
 let cells = [...document.getElementsByClassName("cell")];
+
 
 let name = prompt("What's your name?");
 let game = new Game(name);
@@ -9,6 +27,7 @@ cells.forEach((element, index) => {
     element.innerText = game.board[index];
   }
 });
+
 
 domControls.addListenerToCells(cells);
 
