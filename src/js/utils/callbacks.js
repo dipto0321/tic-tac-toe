857ce133d2088eb34a93316d5810c57637e575df
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-28 17:31:41 +0800

    Include conditoinals on box clickHandler

--- src/js/utils/callbacks.js
+++ src/js/utils/callbacks.js
@@ -3,8 +3,12 @@ function clickHandler(e) {
     const humanChoice = Number(e.target.id);
     const computerChoice = game.turn(humanChoice, game.level);
     e.target.innerText = game.board.grid[humanChoice];
-    document.getElementById(computerChoice).innerText = game.board.grid[computerChoice];
-    document.getElementById(computerChoice).removeEventListener('click', game.handler);
+    if (game.endGame()) removeListenerFromBoxes('click', 'cell', game.handler);
+    if (computerChoice !== undefined) {
+      document.getElementById(computerChoice).innerText = game.board.grid[computerChoice];
+      document.getElementById(computerChoice).removeEventListener('click', game.handler);
+    }
+    if (game.endGame()) removeListenerFromBoxes('click', 'cell', game.handler);
   };
 };
 
